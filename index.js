578267a9e0be5bf96895248c4e91a845e5699fc7
Aleksandr.Denysiuk <Aleksandr.Denysiuk@it-integrator.ua>
2020-10-13 09:59:52 +0300

    Version 2

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package');
+const welcome = require('cli-welcome');
+
+welcome({
+  title: pkgJSON.name,
+  tagLine:  'Get to know Rubydenial',
+  version: pkgJSON.version,
+  description: pkgJSON.description,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
+
 console.log(`
 Rubydenial 
 Rb-Dnl
